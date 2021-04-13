549e89f5cebfef49324cbdd4cc3f4722953cc29e
Ahmed <ahmedtchagnaou@gmail.com>
2021-04-13 20:01:54 +0200

    Correct confusion matrix printing

--- src/kPPV.java
+++ src/kPPV.java
@@ -126,14 +126,18 @@ public class kPPV {
         return classe;
     }
 
+    // Predict all the test data
     private static int[] PredictTestData(Double[] distances){
-
+        // Array to store all the predictions
         int[] predictions = new int[NbClasses*NbExLearning];
-        int index = 0;
+        int index = 0; // Index for the prediction array
 
         for(int classe=0; classe < NbClasses; classe++){
             for(int ex=0; ex < NbExLearning; ex++){
+                // Compute Euclidean Distance for each each example in the test
+                // with each example in the training data
                 ComputeDistances(test[classe][ex], distances);
+                // Get the prediction class with PredictedClass function
                 predictions[index] = PredictedClass(distances);
                 index++;
             }
@@ -142,28 +146,27 @@ public class kPPV {
     }
 
 
+    // Display Precision and Recall metrics and the confusion matrix
     private static void ConfusionMatrix(int[] predictions){
+        // Arrays to store the actual and predicted values for each class
         int[] Iris_setosa = {0,0,0}, Iris_versicolor = {0,0,0}, Iris_virginica = {0,0,0};
-        //int[] Iris_versicolor = {0,0,0};
-        //int[] Iris_virginica = {0,0,0};
-
 
         for(int i=0; i < predictions.length; i++){
-            if(i < 25){
+            if(i < 25){ // Check if it's the first class
                 if(predictions[i] == 0)
                     Iris_setosa[0]+=1;
                 else if(predictions[i] == 1)
                     Iris_versicolor[0]+=1;
                 else
                     Iris_virginica[0]+=1;
-            } else if(i < 50){
+            } else if(i < 50){ // Check if it's the second class
                 if(predictions[i] == 0)
                     Iris_setosa[1]+=1;
                 else if(predictions[i] == 1)
                     Iris_versicolor[1]+=1;
                 else
                     Iris_virginica[1]+=1;
-            } else if(i < 75){
+            } else if(i < 75){ // Check if it's the last class
                 if(predictions[i] == 0)
                     Iris_setosa[2]+=1;
                 else if(predictions[i] == 1)
@@ -173,39 +176,43 @@ public class kPPV {
             }
         }
 
-
-        float Class1Precsion, Class2Precsion, Class3Precsion;
+        // Precision variables for each class
+        float Class1Precision, Class2Precision, Class3Precision;
+        // Recall variables for each class
         float Class1Recall, Class2Recall, Class3Recall;
 
+        // Overall precision and recall variables
         float Precision, Recall;
 
         // Precision
         /*Class1Precsion = Iris_setosa[0]/(IntStream.of(Iris_setosa).sum());
         Class2Precsion = Iris_versicolor[1]/(IntStream.of(Iris_versicolor).sum());
         Class3Precsion = Iris_virginica[2]/(IntStream.of(Iris_virginica).sum());*/
-        Class1Precsion = SafeZero(Iris_setosa, 0);
-        Class2Precsion = SafeZero(Iris_versicolor, 1);
-        Class3Precsion = SafeZero(Iris_virginica, 2);
 
 
-        Precision = (Class1Precsion+ Class2Precsion + Class3Precsion) / 3;
+        Class1Precision = SafeZero(Iris_setosa, 0);
+        Class2Precision = SafeZero(Iris_versicolor, 1);
+        Class3Precision = SafeZero(Iris_virginica, 2);
+
+        Precision = (Class1Precision+ Class2Precision + Class3Precision) / 3;
 
         // Recall
         /*Class1Recall = Iris_setosa[0]/(Iris_setosa[0]+Iris_versicolor[0]+Iris_virginica[0]);
         Class2Recall = Iris_versicolor[1]/(Iris_setosa[1]+Iris_versicolor[1]+Iris_virginica[1]);
         Class3Recall = Iris_virginica[2]/(Iris_setosa[2]+Iris_versicolor[2]+Iris_virginica[2]);*/
+
+
         Class1Recall = SafeZero(Iris_setosa, Iris_versicolor, Iris_virginica, 0);
         Class2Recall = SafeZero(Iris_versicolor, Iris_setosa, Iris_virginica, 1);
         Class3Recall = SafeZero(Iris_virginica, Iris_versicolor, Iris_setosa, 2);
-
 
         Recall = (Class1Recall+ Class2Recall + Class3Recall) / 3;
 
         System.out.println("\nPRECISION\n");
 
-        System.out.println("Classe 1 Precision : "+ Class1Precsion);
-        System.out.println("Classe 2 Precision : "+ Class2Precsion);
-        System.out.println("Classe 3 Precision : "+ Class3Precsion);
+        System.out.println("Classe 1 Precision : "+ Class1Precision);
+        System.out.println("Classe 2 Precision : "+ Class2Precision);
+        System.out.println("Classe 3 Precision : "+ Class3Precision);
         System.out.println("\n Overall Precision : "+ Precision);
 
         System.out.println("\nRECALL\n");
@@ -215,16 +222,24 @@ public class kPPV {
         System.out.println("Classe 3 Recall : "+ Class3Recall);
         System.out.println("\nOverall Recall : "+ Recall);
 
+        // Confusion matrix
         System.out.println("\n\nCONFUSION MATRIX");
 
         System.out.println("                        Actual               ");
         System.out.println("            --------------------------------");
         System.out.println(" Predicted | Classe 0 | Classe 1 | Classe 2 ");
         System.out.println("--------------------------------------------");
-        for(int i=0;i <NbClasses; i++){
+        /*for(int i=0;i <NbClasses; i++){
             System.out.println("| Classe "+i+" |    "+Iris_setosa[i]+"     |     "+Iris_versicolor[i]+"    |     "+Iris_virginica[i]+"    ");
             System.out.println("--------------------------------------------");
-        }
+        }*/
+        System.out.println("| Classe "+0+" |    "+Iris_setosa[0]+"     |     "+Iris_setosa[1]+"    |     "+Iris_setosa[2]+"    ");
+        System.out.println("--------------------------------------------");
+        System.out.println("| Classe "+1+" |    "+Iris_versicolor[0]+"     |     "+Iris_versicolor[1]+"    |     "+Iris_versicolor[2]+"    ");
+        System.out.println("--------------------------------------------");
+        System.out.println("| Classe "+2+" |    "+Iris_virginica[0]+"     |     "+Iris_virginica[1]+"    |     "+Iris_virginica[2]+"    ");
+        System.out.println("--------------------------------------------");
+
 
     }
 
